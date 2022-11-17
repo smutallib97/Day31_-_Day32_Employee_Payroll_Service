042c817b69e1adf602f2b3482b1f00648dfa32fd
smutallib97 <smutallib97@gmail.com>
2022-11-17 23:08:09 +0530

    Added: retrieve all the employee payroll data

--- EmployeePayrollService.sql
+++ EmployeePayrollService.sql
@@ -5,3 +5,4 @@ create table employee_payroll (id INT unsigned NOT NULL AUTO_INCREMENT, name VAR
 salary Double NOT NULL, start DATE NOT NULL, PRIMARY KEY (id));
 Select * from payroll_service.employee_payroll;
 insert into employee_payroll (name, salary, start) values ( 'bill',100000.00,'2018-01-03'),('terisa',200000.00,'2019-11-13');
+select * from employee_payroll;
